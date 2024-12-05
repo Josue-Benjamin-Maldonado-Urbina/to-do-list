a8731014c83c5596de2f2b78ff4ea8669232a19c
Josue-Benjamin-Maldonado-Urbina <120042406+Josue-Benjamin-Maldonado-Urbina@users.noreply.github.com>
2024-12-05 16:13:12 -0600

    complete 1.0

--- app.py
+++ app.py
@@ -3,33 +3,39 @@ from flask_cors import CORS
 from models import db, Task
 
 app = Flask(__name__)
-CORS(app)  # Permitir solicitudes desde el frontend
+CORS(app)
 
 # Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/jbmal/OneDrive/Escritorio/to-do list-backend/backend/instance/task.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-# Crear la base de datos (si no existe)
 with app.app_context():
     db.create_all()
 
-# Endpoint para obtener todas las tareas
+# obtener todas las tareas
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
     tasks = Task.query.all()
     return jsonify([{'id': task.id, 'title': task.title, 'description': task.description, 'completed': task.completed} for task in tasks])
 
-# Endpoint para crear una nueva tarea
+# crear una nueva tarea
 @app.route('/tasks', methods=['POST'])
 def create_task():
     data = request.get_json()
     new_task = Task(title=data['title'], description=data['description'])
     db.session.add(new_task)
     db.session.commit()
-    return jsonify({'message': 'Task created'}), 201
+    
+    # Retornar la nueva tarea con el id generado
+    return jsonify({
+        'id': new_task.id,  # Devolver el id generado
+        'title': new_task.title,
+        'description': new_task.description,
+        'completed': new_task.completed
+    }), 201
 
-# Endpoint para actualizar el estado de una tarea
+# actualizar el estado de una tarea
 @app.route('/tasks/<int:id>', methods=['PUT'])
 def update_task(id):
     task = Task.query.get_or_404(id)
@@ -37,7 +43,7 @@ def update_task(id):
     db.session.commit()
     return jsonify({'message': 'Task updated'})
 
-# Endpoint para eliminar una tarea
+# eliminar una tarea
 @app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
     task = Task.query.get_or_404(id)
